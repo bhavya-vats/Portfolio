8c303f4a6f4cf8496b59104e197ab74933cc6a3d
Bhavya <bhavya.vats05@gmail.com>
2023-05-23 12:55:30 +0530

    first

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className=''>
+        <div>
+            <div>
+                <p>About</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo suscipit neque dolor magni reiciendis fuga porro odio debitis facere? Ex blanditiis, doloremque tempore saepe eum suscipit aliquid odio sequi! Unde.</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam nemo tempora cum fuga nesciunt commodi dignissimos officia sint modi. Magnam soluta, sit sapiente expedita distinctio quibusdam vitae modi velit iste?</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
