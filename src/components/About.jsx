4220b144e32fe573186850815c8e635a2eeddea5
Bhavya <bhavya.vats05@gmail.com>
2023-05-23 13:39:39 +0530

    change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className=''>
-        <div>
-            <div>
-                <p>About</p>
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo suscipit neque dolor magni reiciendis fuga porro odio debitis facere? Ex blanditiis, doloremque tempore saepe eum suscipit aliquid odio sequi! Unde.</p>
+            <p className='text-xl mt-20'>Welcome to my portfolio! I'm a passionate front-end developer dedicated to creating engaging and interactive web experiences. With a strong foundation in HTML, CSS, and JavaScript, I specialize in translating design concepts into pixel-perfect, responsive websites. I thrive on the challenge of crafting intuitive user interfaces that not only look visually appealing but also deliver seamless functionality. </p>
             <br/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam nemo tempora cum fuga nesciunt commodi dignissimos officia sint modi. Magnam soluta, sit sapiente expedita distinctio quibusdam vitae modi velit iste?</p>
+            <p className='text-xl'>Leveraging my creativity and attention to detail, I aim to build user-centric websites that leave a lasting impression. Continuously staying up to date with the latest industry trends and technologies, I am committed to delivering cutting-edge solutions that enhance user experiences. Let's collaborate and bring your vision to life on the web!</p>
         </div>
     </div>
   )
