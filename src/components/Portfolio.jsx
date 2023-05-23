a5dd6e5586dae99604c16593e34236c9f88f3827
Bhavya <bhavya.vats05@gmail.com>
2023-05-23 13:42:16 +0530

    chnge

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import HandSign from '../assets/portfolio/handsign.jpg';
+import Wedding from '../assets/portfolio/wedplanning.jpg';
+import Slider from '../assets/portfolio/thumbnailslider.png';
+import Kbc from '../assets/portfolio/kbc.jpg';
 
 
 const Portfolio = () => {
